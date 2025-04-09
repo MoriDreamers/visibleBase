c8ed6d41f1104527d4ca160b9d361ce46b00f072
MoriDreamers <louhaoyu@outlook.com>
2025-04-09 15:39:26 +0800

    添加POD

--- controllers/pod/create.go
+++ controllers/pod/create.go
@@ -1,13 +1,41 @@
 package pod
 
 import (
+	"context"
+	"visibleBase/config"
+	"visibleBase/controllers"
 	"visibleBase/utils/logs"
 
 	"github.com/gin-gonic/gin"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Create(r *gin.Context) {
 	logs.Info(nil, "pod")
-	//returnData := config.NewReturnData()
+	var pod corev1.Pod
+	returnData := config.NewReturnData()
+	clientset, basicInfo, err := controllers.Basicinit(r, &pod)
+	if err != nil {
+		msg := err.Error()
+		returnData.Status = 401
+		returnData.Message = msg
+		r.JSON(200, returnData)
+		return
+	}
 
+	//clientset的Create会自动读取namespace.Name的值 然后创建一个namespace
+	_, err = clientset.CoreV1().Pods(basicInfo.Namespace).Create(context.TODO(), &pod, metav1.CreateOptions{})
+
+	if err != nil {
+		msg := "创建pod失败" + err.Error()
+		returnData.Status = 401
+		returnData.Message = msg
+		r.JSON(200, returnData)
+		return
+	}
+	//返回数据
+	returnData.Status = 200
+	returnData.Message = "创建pod成功"
+	r.JSON(200, returnData)
 }
